b49fa24c5048a39669af7a34ca335ca166328d2e
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-12-05 14:08:35 +0100

    Create script

--- torrent-create.sh
+++ torrent-create.sh
@@ -10,16 +10,16 @@ source "$(cd "$(dirname "$0")" || exit; pwd -P)/utilities.sh"
 
 OUTPUT_FILENAME="$1.torrent"
 
+sudo mv "$TMP_DIR/$1" "$APP_DIR/sources/$1"
+sudo chmod 755 -R "$APP_DIR/sources"
 
-if sudo transmission-create -o "$APP_DIR/torrents/$OUTPUT_FILENAME" -t "$TRACKER_URL_UDP" "$TMP_DIR/$1" > /dev/null 2>&1
+if sudo transmission-create -o "$APP_DIR/torrents/$OUTPUT_FILENAME" -t "$TRACKER_URL_UDP" "$APP_DIR/sources/$1" > /dev/null 2>&1
   # Create the torrents and get the hash
   then
   # Add tier 2 tracker URL
   if sudo transmission-edit -a "$TRACKER_URL_TCP" "$APP_DIR/torrents/$OUTPUT_FILENAME" > /dev/null 2>&1
   then
-    # Move source file to app dir -- TODO: since all the clients are happy to
-    # share a single dir, that dir should be made the sources dir
-    sudo mv "$TMP_DIR/$1" "$APP_DIR/sources/$1"
+    sudo chmod 755 -R "$APP_DIR/torrents"
     sudo chown "$TRANSMISSION_USER":"$TRANSMISSION_GROUP" "$APP_DIR/torrents/$OUTPUT_FILENAME"
     sudo chown "$TRANSMISSION_USER":"$TRANSMISSION_GROUP" "$APP_DIR/sources/$1"
 
